d4d6c580d1cccc128311e506982c33469ad97cd0
jjessel <jjessel84@gmail.com>
2020-08-16 19:53:20 -0700

    Fix issue with image size

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import background from '../images/lol_background.jpg';
 
 const Home: React.FC = () => {
-    return <img src={background} alt="LOL background" />;
+    return (
+        <div className="backgroundImage">
+            <img src={background} alt="LOL background" />;
+        </div>
+    );
 };
 
 export default Home;
