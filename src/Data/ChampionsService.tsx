6ecb12013161c0c774979ab1912c1db8e4fe4242
jjessel <jjessel84@gmail.com>
2020-08-16 21:30:36 -0700

    add get champion method

--- src/Data/ChampionsService.tsx
+++ src/Data/ChampionsService.tsx
@@ -1,10 +1,16 @@
 import { Champion } from '../models/Champion';
 import jsonObj from '../jsonData/champion.json';
+import axios from 'axios';
 
 export default class ChampionsService {
     static champions = (): Champion[] => {
         return Object.values(jsonObj.data).map((item) => {
             return item as Champion;
         });
+    };
+
+    private static champion = (name: string) => {
+        const uri = `http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/${name}.json`;
+        return axios.get(uri);
     };
 }
