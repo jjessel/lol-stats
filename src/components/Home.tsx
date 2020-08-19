3e2c128cd5a900786edce1761139d97225ecdeb7
jjessel <jjessel84@gmail.com>
2020-08-18 20:25:33 -0700

    add text to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import background from '../images/lol_background.jpg';
 import styled from 'styled-components';
 
 const Div = styled.div`
     width: 100%;
     height: 100%;
+    background-image: url('/images/lol_background.jpg');
+    min-height: 800px;
+    padding-top: 32px;
+`;
+
+const Title = styled.div`
+    color: white;
+    font-size: 82px;
+    font-weight: bold;
+    text-shadow: 1px 1px 2px #000000;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+`;
+
+const SubTitle = styled.div`
+    color: white;
+    font-size: 62px;
+    font-weight: bold;
+    text-shadow: 1px 1px 2px #000000;
+    align-items: center;
+    justify-content: center;
+    display: flex;
 `;
 
 const Home: React.FC = () => {
     return (
         <Div>
-            <img src={background} alt="LOL background" />;
+            <Title>League of Legends</Title>
+            <Title>Companion Site</Title>
+            <SubTitle>Built in React</SubTitle>
         </Div>
     );
 };
